f13ab2e9b89d041c7145667082207fb0ef145b28
a-ide <ayano.ide@gmail.com>
2018-10-07 12:38:57 +0900

    add main files for index

---
+++ src/modules/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+import request from 'superagent'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: null
+    }
+  }
+
+  // マウントされる時
+  componentWillMount() {
+    request.get('./data/catList.json')
+      .accept('application/json')
+      .end((err, res) => {
+        this.loadedJSON(err, res)
+      })
+  }
+
+  // データを読み込んだ時
+  loadedJSON(err, res) {
+    if (err) {
+      console.log('JSON読み込みエラー')
+      return
+    }
+    // 状態を更新
+    this.setState({
+      items: res.body
+    })
+  }
+
+  render() {
+    // JSONデータの読み込みが完了してるか
+    if (!this.state.items) {
+      return <div className='App'>現在読み込み中</div>
+    }
+
+    // 読み込んだデータからアイテムを作る
+    const listItem = this.state.items.map(e => {
+      const yellow = e.images.yellowCar ? <p>譲渡会参加</p> : null
+      const debut = !e.status.debut ?  <p>近日デビュー</p> : null
+      return (
+        <div className='list__item' key={e.status.num}>
+          <div className='list__thumb'>
+            <img src={e.images.thumb} alt={e.status.num} />
+          </div>
+          <div className='list__status'>
+            <a href={e.url}>No.{e.status.num}</a>
+            <span>{e.status.sex}</span>
+            <p>{e.status.age}</p>
+            {yellow}
+            {debut}
+          </div>
+        </div>
+      )
+    })
+    return (
+      <div className='list'>
+        {listItem}
+      </div>
+    )
+  }
+}
+
+export default App
