4595520e0c36291454e49e9c3f49d416b948a942
a-ide <ayano.ide@gmail.com>
2018-10-10 22:57:45 +0900

    move files for front to /frontend

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './modules/App.jsx'
+import App from './modules/frontend/App.jsx'
 import './css/list.scss'
 
-
 ReactDOM.render(<App />, document.getElementById('root'));
+
+
