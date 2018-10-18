48825599ef30b9d5c681b8b913e7ac9be8f43f46
a-ide <ayano.ide@gmail.com>
2018-10-19 08:56:40 +0900

    add stopPropagation() to DisplayToggle.js

--- src/modules/frontend/DisplayToggle.js
+++ src/modules/frontend/DisplayToggle.js
@@ -1,6 +1,7 @@
 const DisplayToggle = (target) => {
   document.getElementById(target)
     .addEventListener('click', (e) => {
+      e.stopPropagation()
       e.target.classList.toggle('is-open')
       e.target.nextElementSibling.classList.toggle('is-open')
     })
