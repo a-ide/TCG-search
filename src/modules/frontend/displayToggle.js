132d84f1acb92e4d1ec254bc5455a71ddbc54b65
a-ide <ayano.ide@gmail.com>
2018-10-18 11:19:36 +0900

    delete files for frontend

--- src/modules/frontend/displayToggle.js
+++
@@ -1,9 +0,0 @@
-const DisplayToggle = (target) => {
-  document.getElementById(target)
-    .addEventListener('click', (e) => {
-      e.target.classList.toggle('is-open')
-      e.target.nextElementSibling.classList.toggle('is-open')
-    })
-}
-
-export default DisplayToggle
