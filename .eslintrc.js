025dfa051b0e973444d264b2e7f25c5e4bf6d4d0
a-ide <ayano.ide@gmail.com>
2018-10-20 15:32:09 +0900

    update .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,16 @@
 module.exports = {
   "extends": "airbnb",
   "rules": {
-      "semi": ["error", "never"],
-      "no-shadow": 1,
+    "semi": ["error", "never"],
+    "no-shadow": 1,
+    "arrow-body-style": 1,
+    "arrow-parens": ["error", "as-needed"],
+    "react/destructuring-assignment": [
+      true, "always", { "ignoreClassFields": true }],
+    "react/prop-types": 1,
+    "no-restricted-syntax": 1,
+    "jsx-a11y/label-has-for": 0,
+    "react/no-access-state-in-setstate": 1,
   },
   "env": {
     "browser": true
